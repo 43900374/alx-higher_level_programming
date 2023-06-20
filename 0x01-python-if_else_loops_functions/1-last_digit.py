217a881200a2122058443a80546d4cb66c6939f3
43900374 <cynthia.k.wairimu@gmail.com>
2023-06-20 12:55:17 +0000

    greater or less than

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+
 import random
-number = random.randint(-10000, 10000)#!/usr/bin/python3
+
+number = random.randint(-10000, 10000)
 
 if number < 0:
     num = number % -10
+else:
+    num = number % 10
 
 if num > 5:
     print(f'Last digit of {number} is {num} and is greater than 5')
